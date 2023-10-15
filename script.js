2c229c75726facd09036b470191b4a361665330a
Kris0011 <krishp759@gmail.com>
2023-10-15 22:52:00 +0530

    added components.json to store the data of all components and fetch them into components.html page

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const parentDiv = document.getElementById("component-items");
+
+    fetch("./components.json")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(component => {
+                const newElement = document.createElement('div');
+                newElement.innerHTML = ` <div class="card">
+                <div class="poster"><a href="components/${component.url}"><img
+                            src="${component.imgUrl}" alt="${component.name}" /></a></div>
+                <div class="details">
+                    <h1>${component.name}</h1>
+                    <h2> By : ${component.authorName}</h2>
+                </div>
+            </div>`
+
+            parentDiv.appendChild(newElement);
+            });
+        })    
+})
+
 function searchComponents() {
     const searchInput = document.getElementById("searchInput").value.toLowerCase();
     const cards = document.querySelectorAll(".card");
